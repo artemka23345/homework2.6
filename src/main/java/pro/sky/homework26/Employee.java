7d02ae30c55733077d4500dadb83d8934524421b
Inv <artem@tarabakin.ru>
2023-12-11 13:54:07 +0300

    Added class Employee

---
+++ src/main/java/pro/sky/homework26/Employee.java
@@ -0,0 +1,58 @@
+package pro.sky.homework26;
+
+import java.util.Objects;
+
+public class Employee {
+    private String name;
+    private String surname;
+
+
+    //Constructor
+    public Employee(String name, String patronymic, String surname, int salary, int department) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + getName() + '\'' +
+                ", surname='" + getSurname() + '\'' +
+                '}';
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(name, employee.name) && Objects.equals(surname, employee.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+}
+
+
+
+
